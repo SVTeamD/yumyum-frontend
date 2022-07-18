9bfc39e04e4b0af36811a174b9ccf196abc21ed6
홍성민 <seanhong2000@gmail.com>
2022-07-18 17:46:03 +0900

    feat: Update UI

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,6 @@ import Auth from './hooks/Auth';
 import Profile from './hooks/Profile';
 
 export default function App() {
-  const REST_API_KEY = '[본인 REST API KEY 값]';
-  const REDIRECT_URI = 'http://localhost:3000/oauth/kakao/callback';
-  const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
-
   return (
     <div
       style={{ backgroundColor: '#FFF2EA', overflow: 'auto', height: '100vh' }}
--- src/components/LinkButton.tsx
+++ src/components/LinkButton.tsx
@@ -1,22 +1,61 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Box from '@mui/material/Box';
+import { Button, Grid } from '@mui/material';
+import getWindowDimensions from '../hooks/WindowSize';
+import Typography from '@mui/material/Typography';
 
 interface Props {
   name: string;
-  color: string;
   link: string;
+  image: string;
 }
 
-export default function LinkButton({ color, link, name }: Props) {
+export default function LinkButton({ link, name, image }: Props) {
   return (
-    <div>
-      <Link
-        className="flex flex-wrap items-center px-5 w-22 h-11 text-black"
-        style={{ backgroundColor: color }}
-        to={link}
+    <Link to={link}>
+      <Box
+        sx={{
+          width: getWindowDimensions().width * 0.38,
+          height: getWindowDimensions().width * 0.51,
+          boxShadow: '1px 3px 5px 5px #9E9E9E',
+          borderRadius: '8%',
+          backgroundColor: '#c4c4c4',
+          '&:hover': {
+            // backgroundColor: '#2e2f2f',
+            opacity: [0.9, 0.8, 0.7]
+          }
+        }}
       >
-        {name}
-      </Link>
-    </div>
+        <Box
+          sx={{
+            width: getWindowDimensions().width * 0.38,
+            height: getWindowDimensions().width * 0.38,
+            borderRadius: '8%',
+            boxShadow: '1px 3px 5px 5px #9E9E9E',
+            backgroundImage: `url(${image})`,
+            backgroundSize: 'contain',
+            backgroundColor: '#fff2ea',
+            '&:hover': {
+              // backgroundColor: '#2e2f2f',
+              opacity: [0.9, 0.8, 0.7]
+            }
+          }}
+        ></Box>
+        <Typography
+          align="center"
+          sx={{
+            width: getWindowDimensions().width * 0.3,
+            height: getWindowDimensions().width * 0.13,
+            fontFamily: 'Gungseo',
+            fontSize: getWindowDimensions().width * 0.052,
+            margin: '0 auto',
+            marginTop: '1.5rem'
+          }}
+        >
+          {name}
+        </Typography>
+      </Box>
+    </Link>
   );
 }
