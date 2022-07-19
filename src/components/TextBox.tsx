d2baaa9877ed971dab4528a1fbba592d7761a6b3
kimich <ickim1218@gmail.com>
2022-07-19 22:49:55 +0900

    TextBox

--- src/components/TextBox.tsx
+++ src/components/TextBox.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import TextField from '@mui/material/TextField';
+import { height } from '@mui/system';
 
 export default function TextBox() {
   return (
     <>
-      <h1>TEXT</h1>
-      <input type="text" placeholder="입력하세요" />
+      <TextField
+        label="가게 이름 입력"
+        id="가게 이름 입력"
+        sx={{ width: '15rem', height: '3.75rem' }}
+      />
     </>
   );
 }
