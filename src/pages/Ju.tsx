40e62b4cedf87213035844bd852e721f255fd9d0
juwon5272 <jackson5272@naver.com>
2022-07-28 23:16:43 +0900

    feat: test crop page

--- src/pages/Ju.tsx
+++ src/pages/Ju.tsx
@@ -21,7 +21,7 @@ import { Typography } from '@mui/material';
 import home from '../assets/images/home.png';
 import { Height } from '@mui/icons-material';
 
-export default function Mode() {
+export default function Ju() {
   const [auth, setAuth] = React.useState(true);
   const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
 
@@ -38,7 +38,7 @@ export default function Mode() {
   };
 
   return (
-    <div>
+    <div className="h-5/7 overflow-y-scroll">
       <Box sx={{ flexGrow: 1 }}>
         <AppBar position="static" style={{ background: '#fff2ea' }}>
           <Toolbar
@@ -87,10 +87,132 @@ export default function Mode() {
             )}
           </Toolbar>
         </AppBar>
+
+        <Box
+          sx={{
+            marginTop: '1rem',
+            marginLeft: '1.5rem',
+            display: 'flex',
+            alignContent: 'left',
+            justifyContent: 'left',
+            fontSize: '1.5rem'
+          }}
+        >
+          <div>"가게 이름이 뭐라구유?"</div>
+        </Box>
+        {/* <Box
+        component="span"
+        sx={{
+          marginTop: '0.5rem',
+          marginLeft: '1.5rem',
+          marginRight: '1.5rem',
+          fontSize: '1.5rem',
+          border: '1px solid grey',
+          display: 'flex',
+          alignContent: 'center'
+        }}
+      >
+        <div>
+          <JuCrop />
+        </div>
+      </Box> */}
+        <div className="px-4">
+          <JuCrop />
+        </div>
+
+        <Box
+          sx={{
+            display: 'flex',
+            alignContent: 'center',
+            justifyContent: 'center'
+          }}
+        >
+          <CategorySelect></CategorySelect>
+        </Box>
+        <Box
+          sx={{
+            marginTop: '1rem',
+            marginLeft: '1.5rem',
+            display: 'flex',
+            alignContent: 'left',
+            justifyContent: 'left',
+            fontSize: '1.5rem'
+          }}
+        >
+          <div>"차림표 추가해봐유!"</div>
+        </Box>
+        {/* <Box
+        component="span"
+        sx={{
+          marginTop: '0.5rem',
+          marginLeft: '1.5rem',
+          marginRight: '1.5rem',
+          fontSize: '1.5rem',
+          border: '1px solid grey',
+          display: 'flex',
+          alignContent: 'center',
+          justifyContent: 'center'
+        }}
+      >
+        <div>
+          <JuCrop />
+        </div>
+      </Box> */}
+        <div className="px-4">
+          <JuCrop />
+        </div>
+        <Box
+          sx={{
+            marginTop: '0.5rem',
+            display: 'flex',
+            alignContent: 'center',
+            justifyContent: 'center',
+            fontSize: '1.5rem'
+          }}
+        >
+          <div>
+            <div>"가게의 메뉴판을 찍어주세여!</div>
+            <div>"자동으로 메뉴가 등록됩니다!</div>
+          </div>
+        </Box>
+        <div className="fixed bottom-0">
+          <Box
+            sx={{
+              // marginTop: '0.5rem',
+              width: '100%',
+              height: '51%',
+              borderTopLeftRadius: '1rem',
+              borderTopRightRadius: '1rem',
+              border: 'solid 1px',
+              backgroundColor: '#79a7c8'
+            }}
+          >
+            <Grid xs={8} item>
+              <Typography
+                sx={{
+                  width: '100%',
+                  height: '50%',
+                  fontWeight: '800',
+                  padding: '3rem',
+                  display: 'flex',
+                  justifyContent: 'center',
+                  fontSize: '1.8rem'
+                }}
+              >
+                <Box
+                  sx={{
+                    width: 50,
+                    height: 45,
+                    justifyContent: 'center',
+                    backgroundImage: `url(${home})`
+                  }}
+                ></Box>
+                "가게 등록 마치기!"
+              </Typography>
+            </Grid>
+          </Box>
+        </div>
       </Box>
-      <div className="px-4">
-        <JuCrop />
-      </div>
     </div>
   );
 }
